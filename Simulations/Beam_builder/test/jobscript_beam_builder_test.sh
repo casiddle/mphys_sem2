880845f9bdc7d685419cf2238974543e0a542f0d
Jasper Burvill <jasperburvill@gmail.com>
2025-02-20 13:31:33 +0000

    Fixing ContinueBack

--- Simulations/Beam_builder/test/jobscript_beam_builder_test.sh
+++ Simulations/Beam_builder/test/jobscript_beam_builder_test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash --login
 #$ -cwd                 # Job will run in the current directory (where you ran qsub)
-#$ -pe amd.pe 32        # Choose a PE name from the tables below and a number of cores
-#$ -l s_rt=2:00:00     # Sets job time limit
+#$ -l short     # Sets job time limit
 #$ -N qv3d              # Set the job's name
 
 # Load any required modulefiles
+source /etc/profile
+
+module purge
 module load libs/intel-18.0/hdf5/1.10.5_mpi
 module load mpi/intel-18.0/openmpi/4.0.1
+module load apps/anaconda3/5.2.0/bin
+
+export PATH=$HOME/.local/bin:$PATH
 
 
 # Inform app how many cores to use
@@ -15,7 +20,7 @@ module load mpi/intel-18.0/openmpi/4.0.1
 #mpirun qv3dMPIX.e v.ini
 
 # Create the witness beam
-python pseedpar.py 35 1602 3.14e14 1 0.1 2e5 > wit.dat
+python pseedpar.py 60 120 7e14 5.75 2.0 2e5 > wit.dat 2>&1
 
 # For MPI applications (small jobs on a single node or larger jobs across multiple compute nodes)
-mpirun -n $NSLOTS qv3dMPIX.e v_high_res_witness.ini
+mpirun -n $NSLOTS qv3dMPIX.e v_beam_builder_test.ini
