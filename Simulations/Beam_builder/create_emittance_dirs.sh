3a2ba467baa89a22cbe48ad8d7ea17a5a48eb9fb
casiddle <carys@novusatus.com>
2025-02-25 11:04:04 +0000

    updated bash script so it submit job

--- Simulations/Beam_builder/create_emittance_dirs.sh
+++ Simulations/Beam_builder/create_emittance_dirs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 for em in 1 2 3; do
-  dir=em${em}
-  mkdir ${dir}
-  sed "s/EMITTANCE/${em}/g" base.txt > ${dir}/new.txt
+  dir="em${em}"
+  mkdir -p ${dir}  # Ensure directory exists
+  sed "s/EMITTANCE/${em}/g" base_jobscript.sh > ${dir}/jobscript.sh
+  cd ${dir}
+  
+  
+  # Submit the job
+  sbatch jobscript.sh  
+  
+  cd ..
 done
-
