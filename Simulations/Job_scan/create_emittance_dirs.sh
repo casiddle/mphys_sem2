82df47b8e9f7eb970e8f87b9626c2fbce16e3f48
casiddle <carys@novusatus.com>
2025-02-25 11:20:48 +0000

    made all emmitance directories go into a parent emittance scan directory

--- Simulations/Job_scan/create_emittance_dirs.sh
+++ Simulations/Job_scan/create_emittance_dirs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Create the parent directory
+mkdir -p emittance_scan
+
 for em in 1 2 3; do
-  dir="em${em}"
+  dir="emittance_scan/em${em}"  # Store emX folders inside emittance_scan
   mkdir -p ${dir}  # Ensure directory exists
   sed "s/EMITTANCE/${em}/g" base_jobscript.sh > ${dir}/jobscript.sh
   cd ${dir}
   
-  
   # Submit the job
   sbatch jobscript.sh  
   
-  cd ..
+  cd ../..  # Move back to the original directory
 done
